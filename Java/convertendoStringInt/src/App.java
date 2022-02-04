f0b66c145598e372c620e6dc160a9f4eb805cca8
Mohammed Ali Scatolino <mohammedscatolino@gmail.com>
2022-02-04 17:35:53 -0300

    segundaConversao

--- Java/convertendoStringInt/src/App.java
+++ Java/convertendoStringInt/src/App.java
@@ -1,7 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int numero = 8;
-        String registro = Integer.toString(numero);
-        System.out.println(registro);
+        /*  int numero = 8;
+         *   String registro = Integer.toString(numero);
+         *   System.out.println(registro); */
+
+        String registro = "8"; 
+        int numero = Integer.parseInt(registro);
+        System.out.println(numero);
     }
 }
