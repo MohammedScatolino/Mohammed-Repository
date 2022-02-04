59b738e230e704b8480f08140d2001b315360b90
Mohammed Ali Scatolino <mohammedscatolino@gmail.com>
2022-02-04 17:31:35 -0300

    exercicioCoversaoStringInt

---
+++ Java/convertendoStringInt/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = 8;
+        String registro = Integer.toString(numero);
+        System.out.println(registro);
+    }
+}
