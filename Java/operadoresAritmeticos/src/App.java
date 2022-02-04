53fdfac6e04a912dce2ed4a1feff66239735b55a
Mohammed Ali Scatolino <mohammedscatolino@gmail.com>
2022-02-04 18:58:49 -0300

    operadoresAritmeticos

---
+++ Java/operadoresAritmeticos/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int n1 = 3;
+        int n2 = 7;
+        float media = (n1+n2)/2;
+        System.out.print("A média entre " +n1 + " e " + n2 + " é " + media +"\n"); 
+
+        
+    }
+}
