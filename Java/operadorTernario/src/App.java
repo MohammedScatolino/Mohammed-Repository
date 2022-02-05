fe160615eaa56419729d9da52e80471230c1a927
Mohammed Ali Scatolino <mohammedscatolino@gmail.com>
2022-02-05 19:32:09 -0300

    exercicioOperadoresLogicos

---
+++ Java/operadorTernario/src/App.java
@@ -0,0 +1,15 @@
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int n1, n2 ,r;
+        n1 = 4;
+        n2 = 3;
+        r = (n1>n2)?n1:n2;
+        System.out.println(r);
+
+
+
+
+    }
+}
