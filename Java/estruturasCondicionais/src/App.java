a7887c52817228235c3657a4405f9a8726f919a0
Mohammed Ali Scatolino <mohammedscatolino@gmail.com>
2022-02-05 20:42:45 -0300

    segundoExercicio

--- Java/estruturasCondicionais/src/App.java
+++ Java/estruturasCondicionais/src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner teclado = new Scanner(System.in);
+       /* Scanner teclado = new Scanner(System.in);
         System.out.println("Digite a nota 1");
         float n1 = teclado.nextFloat();
         System.out.println("Digite a nota 1");
@@ -12,8 +12,20 @@ public class App {
         if  (m >= 7) {
             System.out.println("EXCELENTE!");
         }
-        System.out.printf("Sua média foi " + m );
+        System.out.printf("Sua média foi " + m ); */
 
+
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite a idade:");
+        int idade = teclado.nextInt();
+        teclado.close();
+        if (idade >= 18) {
+            System.out.println("Maior de idade");
+        }
+            else {
+                System.out.println("Menor de idade");
+            }
 
 
 
