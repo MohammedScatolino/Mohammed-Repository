557e868e11ad7284d13c54a761c5585426cb7b43
Mohammed Ali Scatolino <mohammedscatolino@gmail.com>
2022-02-05 19:58:22 -0300

    segundoExercicio

--- Java/operadorTernario/src/App.java
+++ Java/operadorTernario/src/App.java
@@ -2,14 +2,20 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int n1, n2 ,r;
+        /*int n1, n2 ,r;
         n1 = 4;
         n2 = 3;
         r = (n1>n2)?n1:n2;
-        System.out.println(r);
+        System.out.println(r);*/
 
 
 
+        String nome1, nome2, nome3, resposta;
+        nome1 = "Mohammed";
+        nome2 = "Mohammed";
+        nome3 = new String ("Mohammed");
+        resposta = (nome1.equals(nome3))?"igual":"diferente";
+        System.out.println(resposta);
 
     }
 }
