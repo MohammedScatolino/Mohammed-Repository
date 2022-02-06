2c95b575931adaccfeae8c7e33b34c965fd30b3b
Mohammed Ali Scatolino <mohammedscatolino@gmail.com>
2022-02-06 19:17:58 -0300

    segundoExercicio

--- Java/vetores/src/App.java
+++ Java/vetores/src/App.java
@@ -1,9 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int n[] = {6,3,2,9,8};
+       /*int n[] = {6,3,2,9,8};
         for (int c = 0; c<=5; c++){
-            System.out.println(n[c]);
+            System.out.println(n[c]);*/
+
+
+
+            String mes[] = {"Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"};
+            int dias[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            for (int c = 1; c<12; c++){
+                System.out.println(mes[c] + " tem " + dias[c] + " dias");
+            }
+            
         }
 
-    }
+
 }
