30be653e1b5960402e5d7c3047802557cc736995
Mohammed Ali Scatolino <mohammedscatolino@gmail.com>
2022-02-06 19:57:42 -0300

    segundoExercicio

--- Java/metodos/src/App.java
+++ Java/metodos/src/App.java
@@ -1,12 +1,17 @@
 public class App {
-    static void soma (int a, int b) {
+    /*static void soma (int a, int b) {
         int s = a + b;
-        System.out.println(s);
+        System.out.println(s);*/
+        static int soma (int a, int b) {
+            int s = a + b;
+            return s;
     }
     public static void main(String[] args) throws Exception {
         int x = 10;
         int y = 20;
-        soma(x,y);
+        int s = soma(x,y);
+        System.out.println(s);
+
 
         
 
