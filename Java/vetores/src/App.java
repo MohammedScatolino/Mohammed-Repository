9bd0dbb675ddb2beab56ea13ed2a7301a0046190
Mohammed Ali Scatolino <mohammedscatolino@gmail.com>
2022-02-06 19:07:12 -0300

    primerioExercicio

---
+++ Java/vetores/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int n[] = {6,3,2,9,8};
+        for (int c = 0; c<=5; c++){
+            System.out.println(n[c]);
+        }
+
+    }
+}
