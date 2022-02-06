322ad546317ef5bd7f7c5680ac170056f0563b83
Mohammed Ali Scatolino <mohammedscatolino@gmail.com>
2022-02-06 19:52:41 -0300

    exercicioMetodos

---
+++ Java/metodos/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    static void soma (int a, int b) {
+        int s = a + b;
+        System.out.println(s);
+    }
+    public static void main(String[] args) throws Exception {
+        int x = 10;
+        int y = 20;
+        soma(x,y);
+
+        
+
+
+
+
+    }
+}
