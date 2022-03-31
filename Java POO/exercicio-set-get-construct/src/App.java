26d2e863568436a788c0a93049e0362f2f09a681
Mohammed Ali Scatolino <mohammedscatolino@gmail.com>
2022-03-31 20:57:20 -0300

    att

---
+++ Java POO/exercicio-set-get-construct/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+
+       Caneta c1 = new Caneta();
+       c1.setModelo("BIC");
+       System.out.println(c1.getModelo());
+
+    }
+    
+}
