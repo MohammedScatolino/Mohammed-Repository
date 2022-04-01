4cdf69d12b64a398b83488fa8ef9204f92dda767
Mohammed Ali Scatolino <mohammedscatolino@gmail.com>
2022-03-31 21:39:45 -0300

    metodo-construtor-01

--- Java POO/exercicio-set-get-construct/src/App.java
+++ Java POO/exercicio-set-get-construct/src/App.java
@@ -2,9 +2,7 @@ public class App {
     public static void main(String[] args) {
 
        Caneta c1 = new Caneta();
-       c1.setModelo("BIC");
-       System.out.println(c1.getModelo());
-
+       c1.status();
     }
     
 }
--- Java POO/exercicio-set-get-construct/src/Caneta.java
+++ Java POO/exercicio-set-get-construct/src/Caneta.java
@@ -1,7 +1,16 @@
 
 public class Caneta {
-    public String modelo;
+    private String modelo;
+    private String cor;
     private float ponta;
+    private boolean tampada;
+
+    public Caneta() {
+        setModelo("Macaco");
+        setCor("Preto");
+        setPonta(0.7f);
+        tampar();
+    }
 
     public String getModelo() {
         return modelo;
@@ -11,6 +20,14 @@ public class Caneta {
         this.modelo = m;
     }
 
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String c) {
+        this.cor = c;
+    }
+
     public float getPonta() {
         return ponta;
     }
@@ -18,5 +35,19 @@ public class Caneta {
     public void setPonta(float p) {
         this.ponta = p;
     }
-    
+
+    public void tampar(){
+        this.tampada = true;
+    }
+
+    public void destampar(){
+        this.tampada = false;
+    }
+
+    public void status() {
+        System.out.println("Modelo = " + this.modelo); 
+        System.out.println("Cor = " + this.cor);    
+        System.out.println("Ponta = " + this.ponta);
+        System.out.println("Tampada = " + this.tampada);
+    }
 }
